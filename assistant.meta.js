832703372131077f686130fbd0c8e24a70d6b9a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-06-23 14:09:40 +0000

    Deploy 4-life/AdguardAssistant to github.com/4-life/AdguardAssistant.git:gh-pages

---
+++ assistant.meta.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name Adguard Assistant Dev
+// @name:ru Помощник Adguard Dev
+// @name:uk Помічник Adguard Dev
+// @name:de Adguard-Assistent Dev
+// @name:sr Koristi Adguard-ovog pomoćnika Dev
+// @name:pl Asystent Adguarda Dev
+// @name:zh-cn 使用 Adguard 助手 Dev
+// @name:it Assistente Adguard Dev
+// @name:es Asistente de Adguard Dev
+// @name:id Asisten Aguard Dev
+// @namespace Adguard
+// @description Provides easy and convenient way to manage filtering right from the browser
+// @description:ru Позволяет легко и быстро управлять фильтрацией прямо из браузера
+// @description:uk Забезпечує легкий і зручний спосіб керування фільтрацією прямо з веб-переглядача
+// @description:de Bietet einen einfachen und nützlichen Weg die Filterung direkt im Browser zu verwalten
+// @description:sr Pruža lak i zgodan način za upravljanje filtriranjem direktno iz pregledača.
+// @description:pl Zapewnia łatwy i wygodny sposób na zarządzanie filtrowaniem bezpośrednio z przeglądarki
+// @description:zh-cn 提供简单方便的方法来管理浏览器筛选
+// @description:it Fornisce un modo facile e comodo per gestire il filtraggio direttamente dal browser
+// @description:es Permite fácilmente gestionar los filtros desde el navegador
+// @description:id Menyediakan cara mudah dan nyaman untuk mengelola penyaringan langsung dari peramban
+// @version 4.0.4
+// @downloadURL https://AdguardTeam.github.io/AdguardAssistant/assistant.user.js
+// @updateURL https://AdguardTeam.github.io/AdguardAssistant/assistant.meta.js
+// @include *
+// @exclude *://mil.ru/*
+// @exclude *://feedly.com/*
+// @exclude *wikipedia.org*
+// @exclude *icloud.com*
+// @exclude *hangouts.google.com*
+// @exclude *www.facebook.com/plugins/сomments*
+// @exclude *disqus.com/embed/comments*
+// @exclude *vk.com/widget_comments*
+// @exclude *www.youtube.com/embed/*
+// @grant   GM_getValue
+// @grant   GM_setValue
+// @grant   GM_getResourceText
+// @grant   GM_addStyle
+// @grant   property:settings
+// ==/UserScript==
